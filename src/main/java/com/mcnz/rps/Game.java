d2a6f1a889e3ca6fd722f24bdfdb2bcc3d1b550e
jshrivastava-afk <78849262+jshrivastava-afk@users.noreply.github.com>
2022-06-28 13:41:07 +0530

    Update Game.java

--- src/main/java/com/mcnz/rps/Game.java
+++ src/main/java/com/mcnz/rps/Game.java
@@ -8,6 +8,7 @@ public class Game {
 	public static void main(String[] args) {
 		String input = JOptionPane.showInputDialog(prompt);
 		System.out.println("You selected: " + input);
+		System.out.println("You selected:");
 	}
 
 }
